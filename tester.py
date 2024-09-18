9dce45b04073feb95dc1d35c00a0dbce6dbb9deb
Benjamin Thomsen <benjaminlindthomsen@gmail.com>
2024-09-18 08:40:53 +0200

    initial

--- tester.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-k = 0
-while True:
-    with open('C:/Users/benja/Desktop/alarm.txt', 'w') as file:
-        file.write(str(k))
-        k = k + 1
-        file.close()
-        sleep(1)
